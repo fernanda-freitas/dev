a3a8d119c2d89f01c2fd5f59c3b567c50379c34d
Fernanda Freitas <idea.fernanda@gmail.com>
2024-05-13 16:32:58 +0100

    Adjustments

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,14 @@
 import '@/styles/main.scss'
-import { Inter, Reddit_Mono, Poppins } from 'next/font/google'
+import { Inter, Reddit_Mono, Poppins, Lato } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 const redditMono = Reddit_Mono({ subsets: ['latin'], weight: ['400'] })
 const poppins = Poppins({ subsets: ['latin'], weight: ['400'] })
+const lato = Lato({ subsets: ['latin'], weight: ['400'] })
 
 export default function App({ Component, pageProps }) {
 	return (
-		<div className={inter.className}>
+		<div className={lato.className}>
 			<Component {...pageProps} />
 		</div>
 	)
