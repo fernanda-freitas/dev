33d8afada6d4cc859b453ba48b3ea07ceb8be223
Fernanda Freitas <idea.fernanda@gmail.com>
2024-05-13 15:28:29 +0100

    test build process

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,5 +3,5 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-	return <div>Dev Teste 2</div>
+	return <div>Dev Teste 3</div>
 }
