9a7399edb6811e48794867d195325693efe41e00
Fernanda Freitas <idea.fernanda@gmail.com>
2024-05-13 15:12:24 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,44 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      375: "375px",
+      414: "414px",
+      640: "640px",
+      768: "768px",
+      874: "874px",
+      991: "991px",
+      1024: "1024px",
+      1140: "1140px",
+      1280: "1280px",
+      1366: "1366px",
+      1440: "1440px",
+      1536: "1536px",
+      1680: "1680px",
+      1920: "1920px",
+      "media-hover": { raw: "(hover: hover) and (pointer: fine)" },
+    },
+    colors: {
+      inherit: "inherit",
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#FFF",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      spacing: {
+        10: "1rem",
+        20: "2rem",
+        30: "3rem",
+        40: "4rem",
+        50: "5rem",
+        60: "6rem",
       },
+      transitionDuration: {
+        DEFAULT: "300ms",
+        400: "400ms",
+      },
+      fontFamily: {},
     },
   },
   plugins: [],
