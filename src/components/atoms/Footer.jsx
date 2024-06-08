21bcb1b562864e0eab82be8848a294f5223c42dd
Fernanda Freitas <idea.fernanda@gmail.com>
2024-06-08 12:00:53 +0100

    Adjustment to theme color

--- src/components/atoms/Footer.jsx
+++ src/components/atoms/Footer.jsx
@@ -2,8 +2,9 @@ export default function Footer() {
 	const year = new Date().getFullYear()
 
 	return (
-		<div className='absolute bottom-16 left-16 right-16'>
-			<span className='copy-uppercase'>@{year}</span>
+		<div className='absolute bottom-16 left-16 right-16 flex items-center gap-x-2'>
+			<span className='copy-default'>Personal website</span>
+			<span className='copy-default'>@{year}</span>
 		</div>
 	)
 }
