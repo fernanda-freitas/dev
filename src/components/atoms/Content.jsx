13cf02baf76f88731e75bb5c04c4d2d155d7d5c0
Fernanda Freitas <idea.fernanda@gmail.com>
2024-06-08 10:45:01 +0100

    Adjust content

--- src/components/atoms/Content.jsx
+++ src/components/atoms/Content.jsx
@@ -6,15 +6,15 @@ export default function Content() {
 		<Grid className='pt-[50vh] flex flex-col gap-y-[100px]'>
 			<div className='col-span-12 640:col-span-8'>
 				<span className='copy-default'>
-					A frontend developer from Brazil, currently based in Portugal, with a strong connection to design due to her
-					background in the field. Specialized in visual and interface design, she is always eager to create meaningful
-					pieces where creativity and excitement harmonize as the main driving forces.
+					A frontend developer from São Paulo - Brazil, currently based in Portugal, with a strong connection to design
+					arising from a background in the field. Specializing in visual and creative development using cutting-edge
+					technologies such as Next.js and React, there is always eagerness to create impactful pieces where excitement
+					and precision harmonize as the main driving forces.
 				</span>
 			</div>
 			<div className='col-span-12 640:col-span-6'>
 				<span className='copy-default'>
-					Worked for different studios and agencies - Currently at{' '}
-					<LinkText href='https://v-a.studio/'>v-a studio</LinkText>
+					Currently at <LinkText href='https://v-a.studio/'>v-a studio</LinkText>
 				</span>
 			</div>
 		</Grid>
--- src/components/atoms/LinkText.jsx
+++ src/components/atoms/LinkText.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function LinkText({ href, children }) {
 	return (
-		<Link href={href} className='copy-uppercase'>
+		<Link href={href} className='link-text copy-uppercase'>
 			{children}
 		</Link>
 	)
