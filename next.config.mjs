3a7ce7194b176807b26fe360faae1c1d7150a2b0
Fernanda Freitas <idea.fernanda@gmail.com>
2025-12-10 08:34:40 +0000

    chantes to github action yml file and next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: '/dev',
   assetPrefix: '/dev/',
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
